e464ac47cd8eb177894361a1adcd66035784b241
Ramnath Kumar <ramnathkumar181@gmail.com>
2020-11-15 15:50:50 +0530

    Added implementation of gac

--- cli/commands.py
+++ cli/commands.py
@@ -8,3 +8,10 @@ import click
 def get_analyzer_config(ctx):
     res = ctx.obj.get_analyzer_configs()
     print(res)
+
+@click.command(
+    short_help="Get current state of `analyzer_config.json` from the IntelOwl instance"
+)
+@click.pass_context
+def gac(ctx):
+    get_analyzer_config(ctx)
